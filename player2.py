343cbd35185d06cb576e187661ecde8db15181ae
Jock Reed <jock.da.rock@gmail.com>
2018-01-22 16:38:46 -0600

    correct python syntax error
    Signed-off-by: Jock Reed <jock.da.rock@gmail.com>

--- player2.py
+++ player2.py
@@ -57,7 +57,7 @@ def post_score(channel):
     print(start)
     brokerMessage = {'Status': 'scored', 'Player': '2', 'Score': 1, 'Data': '0'}
     print("message sent")
-    mqttc.publish("score", brokerMessage)
+    mqttc.publish("score", json.dumps(brokerMessage))
 
 
 def post_speed(channel):
